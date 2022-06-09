778900ddd40eeccd74087dce0a978dac38f15822
Micho-PC <michelhdd27@gmail.com>
2022-06-09 21:08:34 +0300

    added type to user table migration

--- laravel-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->tinyInteger('type');
+            $table->tinyInteger('type')->default(0); //admins are of type 1
             $table->timestamps();
         });
     }
