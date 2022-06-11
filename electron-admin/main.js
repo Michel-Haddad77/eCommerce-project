372cf35209b23e496930dcbef4ed52d26251a943
Micho-PC <michelhdd27@gmail.com>
2022-06-11 21:28:41 +0300

    Created admin main page

--- electron-admin/main.js
+++ electron-admin/main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron')
 function createWindow(){
     const win = new BrowserWindow();
 
-    win.loadFile('index.html')
+    win.loadFile('./admin page/admin.html')
 }
 
 //wait for the app to be ready to launch browser window
