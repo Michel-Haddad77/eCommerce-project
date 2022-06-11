5b4a57ee4ecb09d718e5f57c921b0df7b9d844d4
Micho-PC <michelhdd27@gmail.com>
2022-06-12 01:58:24 +0300

    linked to register api

--- frontend-user/login page/scripts/script.js
+++ frontend-user/login page/scripts/script.js
@@ -49,4 +49,73 @@ let logIn = (e)=>{
     })
   }
 
-  log_in_btn.addEventListener('click', logIn);
+
+  //function when clicking sign up button 
+let signUpSubmit= (e) =>{
+    e.preventDefault();
+    let data = new FormData();
+  
+    data.append('name', document.getElementById("name").value);
+    data.append('email', document.getElementById("email").value);
+    data.append('password', document.getElementById("password").value);
+    data.append('password_confirmation', document.getElementById("confirm-password").value);
+  
+    //linking with sign up api
+    axios({
+      method: 'post',
+      url: 'http://localhost:8000/api/register',
+      data: data,
+    })
+    .then(function (response) {
+      console.log(response.data);
+      alert(response.data.user.name + " has successfully registered! \nPlease login to proceed");
+      //check if the email exists
+    //   if(response.data["success"]){
+    //     window.location.href = "./pages/explore-page/explore.html";
+    //     localStorage.setItem("id", response.data["id"]);
+    //     //id = response.data["id"];
+    //   }else{
+    //     alert(response.data["response"]); //email already exists
+    //   }
+    })
+    .catch(function (error){
+        //In case of Unauthorization or invalidation
+        //saving the data object in a variable
+        let data = error.response.data;
+        let msg = '';
+        
+        //loop over the entire data object to collect all messages
+        Object.keys(data).forEach(key =>{
+            msg = msg + data[key] + '\n';
+        })
+        alert(msg);
+    })
+}
+
+//Event listeners for signup and login
+log_in_btn.addEventListener('click', logIn);
+sign_up_btn.addEventListener('click',signUpSubmit);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sign-up");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+btn.addEventListener("click", function(){
+    modal.style.display = "block";})
+
+// When the user clicks on <span> (x), close the modal
+span[0].addEventListener("click", function(){
+    modal.style.display = "none";})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+}
+}
