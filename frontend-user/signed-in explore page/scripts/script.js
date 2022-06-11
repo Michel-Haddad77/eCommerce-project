16e3081b23ec974a3f12351cdb37fd418e1b1b2b
Micho-PC <michelhdd27@gmail.com>
2022-06-12 02:41:27 +0300

    created axios for getting all products

--- frontend-user/signed-in explore page/scripts/script.js
+++ frontend-user/signed-in explore page/scripts/script.js
@@ -1,21 +1,18 @@
-//linking with login api
-axios({
-  method: 'get',
-  url: 'http://localhost:8000/api/all_products',
-})
-.then(function (response) {
-  //
-})
-.catch(function (error){
-    //In case of Unauthorization or invalidation
-    //saving the data object in a variable
-    let data = error.response.data;
-    let msg = '';
-    
-    //loop over the entire data object to aollect all messages
-    Object.keys(data).forEach(key =>{
-        msg = msg + data[key] + '\n';
-    })
-    alert(msg);
-})
+
+  //linking with login api
+  axios({
+    method: 'get',
+    url: 'http://localhost:8000/api/all_products',
+  })
+  .then(function (response) {
+    let products = response.data.data;
+    for(let i=0; i<products.length; i++){
+      let id = products[i]["id"];
+      let name = products[i]["name"];
+      let avg_cost = products[i]["avg_cost"];
+      let category = products[i]["category"];
+
+      console.log(name);
+    }
+  })
 
