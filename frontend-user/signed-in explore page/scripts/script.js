32e1601f42789fa9f20a311359fe849c28e4a21a
Micho-PC <michelhdd27@gmail.com>
2022-06-12 03:10:30 +0300

    completed linking with get-all-products API

--- frontend-user/signed-in explore page/scripts/script.js
+++ frontend-user/signed-in explore page/scripts/script.js
@@ -1,18 +1,51 @@
 
-  //linking with login api
-  axios({
-    method: 'get',
-    url: 'http://localhost:8000/api/all_products',
-  })
-  .then(function (response) {
-    let products = response.data.data;
-    for(let i=0; i<products.length; i++){
-      let id = products[i]["id"];
-      let name = products[i]["name"];
-      let avg_cost = products[i]["avg_cost"];
-      let category = products[i]["category"];
+//linking with login api
+axios({
+  method: 'get',
+  url: 'http://localhost:8000/api/all_products',
+})
+.then(function (response) {
+  let products = response.data.data;
+  //loop over each product
+  for(let i=0; i<products.length; i++){
+    let id = products[i]["id"];
+    let name = products[i]["name"];
+    let price = products[i]["price"];
+    let category = products[i]["category"];
 
-      console.log(name);
-    }
-  })
+    //call the createProduct function
+    createProduct(id,name,price,category);
+  }
+})
 
+function createProduct(id,name,price, category){
+    //creating product container and inserting it in the all products div
+    const all_products_div = document.getElementById("all-container");
+    const product_div = document.createElement("div");
+    product_div.id = id;
+    product_div.className = "product";
+    all_products_div.appendChild(product_div);
+
+    //creating product img
+    const product_img = document.createElement("img");
+    product_img.src = "../assets/placeholder.jpg";
+    product_img.className = "explore-img";
+    product_div.appendChild(product_img);
+
+    //creating th header h4
+    const product_name = document.createElement("h4");
+    product_name.innerHTML = name;
+    product_div.appendChild(product_name);
+
+    //Creating the category
+    const product_categ = document.createElement("h5");
+    product_categ.innerHTML = category;
+    product_div.appendChild(product_categ);
+
+    //creating the price
+    const product_price = document.createElement("h3");
+    product_price.className = "price";
+    product_price.innerHTML = price +"$";
+    product_div.appendChild(product_price);
+
+}
